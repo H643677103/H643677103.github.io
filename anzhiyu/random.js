f9d2353defded3e8cc4f40b47eae802b966a5601
柠柚 <11838150+handust@user.noreply.gitee.com>
2023-11-29 17:57:09 +0800

    Site updated: 2023-11-29 17:57:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/62bbbb78.html","posts/db88f376.html","posts/81dca98f.html","posts/75882a6b.html","posts/d50a.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/62bbbb78.html","posts/81dca98f.html","posts/db88f376.html","posts/d50a.html","posts/75882a6b.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
