5586c0005b77304e5719f0c44b70e8f5978a603d
H643677103 <112322320+H643677103@users.noreply.github.com>
2024-10-05 08:22:29 +0000

    2 Updated By Github Actions

--- chyt/random.js
+++ chyt/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/62bbbb78.html","posts/75d1f4f5.html","posts/db88f376.html","posts/81dca98f.html","posts/d50a.html","posts/75882a6b.html","posts/70e6f39c.html","posts/ef3414ee.html"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
+var posts=["posts/62bbbb78.html","posts/75d1f4f5.html","posts/81dca98f.html","posts/db88f376.html","posts/d50a.html","posts/75882a6b.html","posts/70e6f39c.html","posts/ef3414ee.html"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
