383185f4ac6d43b2d8db217eba6f8de434c7b34c
H3223610600 <3223610600@qq.com>
2025-02-09 16:21:03 +0800

    Site updated: 2025-02-09 16:20:44

--- chyt/random.js
+++ chyt/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/f96a143d.html","posts/81dca98f.html","posts/62bbbb78.html","posts/db88f376.html","posts/d50a.html","posts/75882a6b.html","posts/70e6f39c.html","posts/ef3414ee.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/62bbbb78.html","posts/f96a143d.html","posts/81dca98f.html","posts/db88f376.html","posts/d50a.html","posts/75882a6b.html","posts/70e6f39c.html","posts/ef3414ee.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
